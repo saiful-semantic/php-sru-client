26df3ce3eae1aafa95d81ddfa742a104a01e38ae
Dan Michael O. Heggø <danmichaelo@gmail.com>
2014-04-26 16:33:51 +0200

    :eyes: Update API docs after commit to master branch

--- api_docs/tree.js
+++ api_docs/tree.js
@@ -1,10 +1,12 @@
 var tree = [["Scriptotek",,"",
                 [["Sru","Scriptotek\/Sru.html","",
                 [["Client","Scriptotek\/Sru\/Client.html","",
-                [        ]],["ExplainResponse","Scriptotek\/Sru\/ExplainResponse.html","",
+                [        ]],["ExplainResponse","Scriptotek\/Sru\/ExplainResponse.html"," < Response",
                 [        ]],["InvalidResponseException","Scriptotek\/Sru\/InvalidResponseException.html"," < Exception",
                 [        ]],["Record","Scriptotek\/Sru\/Record.html","",
                 [        ]],["Records","Scriptotek\/Sru\/Records.html","",
                 [        ]],["Response","Scriptotek\/Sru\/Response.html","",
+                [        ]],["ResponseInterface","Scriptotek\/Sru\/ResponseInterface.html","",
+                [        ]],["SearchRetrieveResponse","Scriptotek\/Sru\/SearchRetrieveResponse.html"," < Response",
                 [        ]]        ]]        ]]        ]
 
